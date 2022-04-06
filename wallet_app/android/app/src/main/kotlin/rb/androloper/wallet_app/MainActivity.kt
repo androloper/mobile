4839afeb45120219eb5ef1e2fdaac46b2dc6875a
Ramazan Baybörek <ramazanbayborek@gmail.com>
2022-04-06 09:36:17 +0300

    Wallet App UI

---
+++ wallet_app/android/app/src/main/kotlin/rb/androloper/wallet_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package rb.androloper.wallet_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ wallet_app/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'card_screen.dart';
+import 'home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const String path = 'lib/src/walletapp_ui/main.dart';
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WalletApp(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class WalletApp extends StatefulWidget {
+  @override
+  _WalletAppState createState() => _WalletAppState();
+}
+
+class _WalletAppState extends State<WalletApp> {
+  var screens = [
+    HomePage(),
+    CardPage(),
+  ];
+
+  int selectedTab = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(38, 81, 158, 1),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.credit_card), label: "Card"),
+        ],
+        onTap: (index) {
+          setState(() {
+            selectedTab = index;
+          });
+        },
+        showUnselectedLabels: true,
+        iconSize: 30,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        elevation: 0,
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: screens[selectedTab],
+    );
+  }
+}
