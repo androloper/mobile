e8531eab08b88e28c3fd65d8fa6db964fcae1a76
Ramazan Baybörek <ramazanbayborek@gmail.com>
2022-04-07 10:15:12 +0300

    Buying Tickets App UI

---
+++ buying_tickets_app/lib/home_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'exhibition_bottom_sheet.dart';
+import 'sliding_cards.dart';
+import 'tabs.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 8),
+                Header(),
+                SizedBox(height: 40),
+                Tabs(),
+                SizedBox(height: 8),
+                SlidingCardsView(),
+              ],
+            ),
+          ),
+          ExhibitionBottomSheet(), //use this or ScrollableExhibitionSheet
+        ],
+      ),
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Text(
+        'Shenzhen',
+        style: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
