674a86096ac78740c375bea6bca1c924b29bb62a
Ramazan Baybörek <61885368+androloper@users.noreply.github.com>
2023-11-12 16:41:39 +0300

    live_tracking_app initials

---
+++ live_tracking_app/android/app/src/main/kotlin/unifortunately/development/live_tracking_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package unifortunately.development.live_tracking_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ live_tracking_app/lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Live Tracking',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SfMaps(
+              layers: [
+                MapShapeLayer(
+                  source: MapShapeSource.asset(
+                    'assets/usa.json',
+                    shapeDataField: 'STATE_NAME',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
