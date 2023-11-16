8de7193b7f43a769a67ec15e5c5d208eea46bdf9
Ramazan Baybörek <61885368+androloper@users.noreply.github.com>
2023-11-16 11:36:22 +0300

    tracking app initial

--- live_tracking_app/lib/main.dart
+++ live_tracking_app/lib/main.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_maps/maps.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+// ctrl + f and then '//.*'
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -30,27 +34,102 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  double lat = 0.0;
+  double lng = 0.0;
+  List<Marker> places = <Marker>[];
+  BitmapDescriptor? carIcon;
+
+  iconMaker(context) {
+    ImageConfiguration config = createLocalImageConfiguration(context);
+    BitmapDescriptor.fromAssetImage(config, 'assets/car.png', ).then((icon)=>{
+      carIcon = icon
+    });
+  }
+
+  // var openedLocation;
+
+  Future<void> getLocation() async {
+    if (await Geolocator.checkPermission() == LocationPermission.denied) {
+      await Geolocator.requestPermission();
+    }
+    var location = await Geolocator.getCurrentPosition(
+        desiredAccuracy:
+            LocationAccuracy.best); //ios ~0m android 0-100m for best accuracy
+    debugPrint(
+        'Latitude: ${location.latitude} \nLongitude: ${location.longitude} \nWhole information: ${location}');
+    setState(() {
+      lat = location.latitude;
+      lng = location.longitude;
+    });
+  }
+
+  Future<void> goLocation() async {
+    GoogleMapController controller = await mapControl.future;
+    var goPlace = CameraPosition(target: LatLng(38.4392077, 27.2064136),zoom:18);
+    var goingPlace = Marker(
+      markerId: const MarkerId('Id'),
+      position: LatLng(lat, lng),
+      infoWindow: const InfoWindow(title: 'RBHome', snippet: 'Home'),
+      icon: carIcon!,
+    );
+    setState(() {
+      places.add(goingPlace);
+    });
+    controller.animateCamera(CameraUpdate.newCameraPosition(goPlace));
+  }
+
+  // @override
+  // void initState() async {
+  //   super.initState();
+  //   getLocation(); // Call getLocation() to set lat and lng
+  //   openedLocation = CameraPosition(target: LatLng(lat, lng));
+  // }
+
+
+  Completer<GoogleMapController> mapControl = Completer();
+  var openedLocation = const CameraPosition(target: LatLng(38.4392077, 27.2064136), zoom: 10);
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    iconMaker(context);
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SfMaps(
-              layers: [
-                MapShapeLayer(
-                  source: MapShapeSource.asset(
-                    'assets/usa.json',
-                    shapeDataField: 'STATE_NAME',
-                  ),
-                ),
-              ],
-            ),
-          ),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          foregroundColor: Theme.of(context).colorScheme.background,
+          title: Text('Live Tracking App'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                width: width,
+                height: height*.5,
+                child: GoogleMap(
+                  initialCameraPosition: openedLocation,
+                  mapType: MapType.normal,
+                  onMapCreated: (GoogleMapController controller){
+                    mapControl.complete(controller);
+                  },
+                  markers: Set<Marker>.of(places),
+                  // trafficEnabled: true,
+                ),
+              ),
+              Text('Latitude: $lat'),
+              Text('Longitude: $lng'),
+              ElevatedButton(
+                  onPressed: () {
+                    getLocation();
+                  },
+                  child: Text('GetLocationInfo')),
+              ElevatedButton(
+                  onPressed: () {
+                    goLocation();
+                  },
+                  child: Text('GoLocation'))
+            ],
+          ),
+        ));
   }
 }
